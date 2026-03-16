99fa2347ee18591b2b7de4d1b4238b479b93e433
nithindcvtp <117925607+nithindcvtp@users.noreply.github.com>
2026-03-16 21:38:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function relayOn(){
+
+console.log(Relay ON);
+
+}
+
+function relayOff(){
+
+console.log(Relay OFF);
+
+}
+
+function addSchedule(){
+
+console.log(Add schedule);
+
+}
+
+function setTimeFromBrowser(){
+
+const now = new Date();
+
+console.log(now);
+
+}
+
+function setManualTime(){
+
+let t = document.getElementById(manualDateTime).value;
+
+console.log(t);
+
+}
